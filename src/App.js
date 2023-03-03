860d56a0aa18a93ad1a70f3e006a174c43bbf624
Anthony Viramontes <amviramontes34@yahoo.com>
2023-03-02 23:15:31 -0800

    removed unnecessary files, called api with axis and user can look up rick n morty characters

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://rickandmortyapi.com/api/character/?name=${query}`
+        );
+        setCharacters(data.results);
+      } catch (error) {
+        console.errer(error);
+      }
+    };
+
+    fetchData();
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <div>Hello world</div> */}
+      <div className="search">
+        <input
+          type="text"
+          placeholder={"Search Character"}
+          className={"input"}
+          onChange={(event) => setQuery(event.target.value)}
+          value={query}
+        />
+      </div>
+      <div className="result">
+        {characters.map((character) => (
+          <div>
+            <img src={character.image} alt={character.name} />
+            <br />
+            {character.name}
+            <br />
+            {character.species}
+            <br />
+            {character.gender}
+            <br />
+            {character.status}
+            <br />
+            {character.id}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
